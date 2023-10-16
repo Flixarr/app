b558012f76df48b40077f2bf28b8682f336dd28d
Marc Hershey <marchershey@users.noreply.github.com>
2023-10-15 23:38:45 -0400

    chore(Dependencies): :arrow_up: added livewire, alpinejs, tailwindcss, and tall toasts

---
+++ Flixarr/tailwind.config.js
@@ -0,0 +1,42 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/css/**/*.css",
+        "./vendor/usernotnull/tall-toasts/config/**/*.php",
+        "./vendor/usernotnull/tall-toasts/resources/views/**/*.blade.php",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                primary: {
+                    dark: colors.blue[700],
+                    DEFAULT: colors.blue[600],
+                    light: colors.blue[500],
+                    muted: colors.blue[300],
+                    lighter: colors.blue[100],
+                    lightest: colors.blue[50],
+                },
+                muted: {
+                    DEFAULT: colors.gray[500],
+                    light: colors.gray[300],
+                    lighter: colors.gray[200],
+                    lightest: colors.gray[100],
+                },
+                gray: colors.slate,
+            },
+        },
+    },
+    variants: {
+        extend: {
+            lineClamp: ["focus"],
+        },
+    },
+    plugins: [
+        require("@tailwindcss/forms"),
+        require("@tailwindcss/typography"),
+    ],
+};
